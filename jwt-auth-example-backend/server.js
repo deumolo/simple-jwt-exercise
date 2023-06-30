bc80c8e1a41b19228d3c2902da8a65b9aa336db2
Daniel <deumolo@gmail.com>
2023-06-30 15:01:42 -0600

    Backend - Created login post route to create token

--- jwt-auth-example-backend/server.js
+++ jwt-auth-example-backend/server.js
@@ -13,3 +13,21 @@ const users = [
   { id: 1, username: 'user1', password: 'password1' },
   { id: 2, username: 'user2', password: 'password2' },
 ];
+
+// Route for user login and JWT generation
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  // Find the user in the fake user data array
+  const user = users.find(
+    (user) => user.username === username && user.password === password
+  );
+
+  if (user) {
+    // Generate and send the JWT token
+    const token = jwt.sign({ userId: user.id }, secretKey, { expiresIn: '1h' });
+    res.json({ token });
+  } else {
+    res.status(401).json({ error: 'Invalid credentials' });
+  }
+});
