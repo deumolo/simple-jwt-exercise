392c24aa902f089721d79f3c05a84ec260ff485f
Daniel <deumolo@gmail.com>
2023-06-30 14:58:03 -0600

    Backend - Added dummy data to server file

--- jwt-auth-example-backend/server.js
+++ jwt-auth-example-backend/server.js
@@ -8,3 +8,8 @@ const secretKey = 'your_secret_key';
 
 app.use(cors());
 app.use(bodyParser.json());
+// Fake user data for demonstration purposes
+const users = [
+  { id: 1, username: 'user1', password: 'password1' },
+  { id: 2, username: 'user2', password: 'password2' },
+];
