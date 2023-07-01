0a81381ed85923253ed1a778606b852e47bfc18f
Daniel <deumolo@gmail.com>
2023-06-30 23:50:22 -0600

    Frontend - Created app component with useState variables

---
+++ jwt-auth-example-frontend/src/App.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+function App() {
+  const [token, setToken] = useState(null);
+  const [username, setUsername] = useState(null);
+  const [password, setPassword] = useState(null);
+
+  return <></>;
+}
+
+export default App;
