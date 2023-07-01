359475350a21dd96d094f003b789b0a5ad924f57
Daniel <deumolo@gmail.com>
2023-06-30 23:52:59 -0600

    Frontend - Created login handler function

--- jwt-auth-example-frontend/src/App.js
+++ jwt-auth-example-frontend/src/App.js
@@ -1,9 +1,27 @@
 import React, { useState } from 'react';
+import axios from 'axios';
+
+const apiUrl = 'http://localhost:5001';
 
 function App() {
+  
   const [token, setToken] = useState(null);
   const [username, setUsername] = useState(null);
   const [password, setPassword] = useState(null);
+
+  const loginHandler = async () => {
+    try {
+      const response = await axios.post(apiUrl + '/login', {
+        username,
+        password,
+      });
+
+      const { token } = response.data;
+      setToken(token);
+    } catch (err) {
+      console.log(err.response.data);
+    }
+  };
 
   return <></>;
 }
